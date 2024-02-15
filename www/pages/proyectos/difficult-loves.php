c65a5d123f68e53ef3d59ad4230a19cf203b8335
Dav Mat <damarev@gmail.com>
2024-02-15 22:18:46 +0100

    Go

--- www/pages/proyectos/difficult-loves.php
+++ www/pages/proyectos/difficult-loves.php
@@ -6,7 +6,8 @@
 
 <div class="max-w-4xl mx-auto px-5 py-6 my-6 prosed">
     <p class="quote text-2xl uppercase">Difficult loves</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus esse eligendi ipsum beatae illo, obcaecati fugiat, reprehenderit error dignissimos voluptatem id. Illum voluptatem ullam ratione voluptas ex omnis dicta iusto.</p>
+    <p>El 13 de marzo de 2020 hice la compra para quince días con la idea de no salir de casa a no ser que fuera estrictamente necesario. Dos días después nos confinaron. Ese día comencé un diario.</p>
+    <p><em>Difficult Loves</em> es un diario íntimo en tiempos de incertidumbre y soledad. Compuesto por 72 páginas, este libro de artista captura las vivencias de una época extraordinaria y desconcertante, marcada por la pandemia de la COVID-19.</p>
 </div>
 
 <div class="flex gap-5 my-6">
@@ -19,7 +20,8 @@
 </div>
 
 <div class="max-w-4xl mx-auto px-5 py-12 prosed">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus esse eligendi ipsum beatae illo, obcaecati fugiat, reprehenderit error dignissimos voluptatem id. Illum voluptatem ullam ratione voluptas ex omnis dicta iusto.</p>
+    <p>Cuando comenzó el confinamiento, pensé que sería una oportunidad para parar y poner un poco de freno al ritmo de vida que llevaba. Aprovechar para hacer en casa todas aquellas cosas para las que no tenía tiempo. Sin embargo, sucedió todo lo contrario y me vi desbordada ante la cantidad de responsabilidades, estímulos y ruido procedentes del exterior.</p>
+    <p>Durante casi 60 días experimenté la falta de contacto físico, algo que las pantallas no pueden suplir. Esta soledad y aislamiento me llevaron de nuevo al collage en el que me refugié cada día como punto de conexión y desconexión con la realidad.</p>
 </div>
 
 <div class="flex gap-5 my-6">
@@ -59,8 +61,9 @@
         <img class="" src="/img/proyectos/difficult-loves/11-difficult-loves.jpg" alt="">
     </div>
     <div class="px-5 flex flex-col justify-center prosed">
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, et consequatur? Nisi quo libero numquam quis quam perferendis minus quae necessitatibus fugiat consequuntur labore iusto corrupti, blanditiis explicabo hic vel.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, perspiciatis asperiores. Eius mollitia possimus perferendis asperiores, quibusdam, consectetur optio doloribus eaque fugiat, accusantium aliquid assumenda? Consequatur consectetur soluta aperiam sint!</p>
+        <p>Así surge <em>Difficult Loves</em>, un diario de cuarentena donde canalizo y expreso mi sentir sobre lo que acontecido. Cada página habla de distancia, de ausencias, de lo que significa un hogar, de lo colectivo y lo íntimo, del pasado y el presente, de la incertidumbre… de nuevos caminos. A diferencia del resto de mi obra, en esta ocasión el proceso fue muy ágil y no dejé reposar las piezas. Buscaba que esta particular manera de documentación fluyera de forma espontánea y directa, como un reflejo puro y libre al que volver cuando todo pasara.</p>
+        <p><em>Difficult Loves</em> se presenta como un documento sobre la singularidad de esos tiempos. Un diario visual donde los fragmentos de imágenes y palabras se entrelazan para narrar la historia y acabar siendo testigo de la resiliencia y creatividad en medio de la dificultad.</p>
+        <p class="text-sm">Esta pieza formó parte del festival de autoedición (h)ójala, que tuvo lugar en abril de 2023 en The Stendhal Room.</p>
     </div>
 </div>
 
